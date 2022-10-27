960b63a5a50c591e62214ab7952e7a95cc1d451f
Davide <DaviR87@users.noreply.github.com>
2022-10-27 17:02:40 +0200

    Modified Lazy Page module

--- src/app/pages/lazy-page/lazy-page.module.ts
+++ src/app/pages/lazy-page/lazy-page.module.ts
@@ -1,9 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { LazyPageComponent } from './lazy-page.component';
-import { LazyChildComponent } from './lazy-child/lazy-child.component';
-import { AnotherLazyChildComponent } from './another-lazy-child/another-lazy-child.component';
 import { SharedModule } from 'src/app/shared/shared.module';
+import { LazyPageComponent } from './lazy-page.component';
 
 const ROUTES: Routes = [
   {
@@ -11,31 +9,15 @@ const ROUTES: Routes = [
     component: LazyPageComponent,
     children: [
       {
-        path: '',
-        pathMatch: 'full',
-        redirectTo: 'lazy-child'
-      },
-      {
         path: 'lazy-child',
         loadChildren: () => import('./lazy-child/lazy-child.module').then(m => m.LazyChildModule)
       },
       {
         path: 'another-lazy-child',
         loadChildren: () => import('./another-lazy-child/another-lazy-child.module').then(m => m.AnotherLazyChildModule)
-      },
-      {
-        path: '**',
-        redirectTo: 'lazy-child'
       }
     ]
   }
-];
-
-const COMPONENTS = [
-  LazyPageComponent
-];
-const DIRECTIVES = [
-  
 ];
 
 @NgModule({
@@ -44,12 +26,10 @@ const DIRECTIVES = [
     RouterModule.forChild(ROUTES)
   ],
   declarations: [
-    ...COMPONENTS,
-    ...DIRECTIVES
+    LazyPageComponent
   ],
   exports: [
-    ...COMPONENTS,
-    ...DIRECTIVES,
+    LazyPageComponent
   ],
   providers: [
   ]
